f54bc4d454a6be9660b8974c7f704f0ae639f140
ryaan-web <abhi28k09@gmail.com>
2020-05-12 17:54:25 +0530

    initial app, HMR and splitting by route

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { hot } from "react-hot-loader/root";
+
+// import  "./index.css";
+
+const  render = (Component) =>
+    ReactDOM.render(<Component />, document.getElementById("root"));
+
+render(hot(App));
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,62 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 30120;
 
 module.exports = {
-  // Webpack configuration goes here
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.[hash].js',
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
+    // Webpack configuration goes here
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[hash].js',
+        publicPath: '/'
+    },
+    resolve: {
+        alias: {
+            "react-dom": "@hot-loader/react-dom",
+        },
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
 
-      // First Rule
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
+        // First Rule
+        {
+            test: /\.(js)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        },
 
-      // Second Rule
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localsConvention: 'camelCase',
-              sourceMap: true
+        // Second Rule
+        {
+            test: /\.css$/,
+            use: [
+            {
+                loader: 'style-loader'
+            },
+            {
+                loader: 'css-loader',
+                options: {
+                modules: true,
+                localsConvention: 'camelCase',
+                sourceMap: true
+                }
             }
-          }
+            ]
+        }
         ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      favicon: 'public/favicon.ico'
-    })
-  ],
-  devServer: {
-    host: 'localhost',
-    port: port,
-    historyApiFallback: true,
-    open: true
-  }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        favicon: 'public/favicon.ico'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    devServer: {
+        host: 'localhost',
+        port: port,
+        historyApiFallback: true,
+        open: true,
+        hot: true
+    }
 };
